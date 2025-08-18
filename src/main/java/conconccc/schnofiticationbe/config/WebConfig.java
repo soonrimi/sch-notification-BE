24f52cb800b8addc1d824b8d68606b2533227f80
iubns <iubns@naver.com>
2025-08-18 23:11:19 +0900

    subscribe

--- src/main/java/conconccc/schnofiticationbe/config/WebConfig.java
+++ src/main/java/conconccc/schnofiticationbe/config/WebConfig.java
@@ -7,13 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://notification.iubns.net/") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
-                .allowCredentials(true) 
-                .addResourceHandler("/uploads/**")
+        registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://notification.iubns.net/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
---
+++ src/main/java/conconccc/schnofiticationbe/dto/SubscribeDto.java
@@ -0,0 +1,42 @@
+package conconccc.schnofiticationbe.dto;
+
+import conconccc.schnofiticationbe.entity.Subscribe;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class SubscribeDto {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateRequest {
+        private String category;
+        private String device;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateRequest {
+        private String category;
+        private String device;
+    }
+
+    @Data
+    public static class Response {
+        private int id;
+        private String category;
+        private String device;
+        private LocalDateTime createdDate;
+
+        public Response(Subscribe subscribe) {
+            this.id = subscribe.getId();
+            this.category = subscribe.getCategory();
+            this.device = subscribe.getDevice();
+            this.createdDate = subscribe.getCreatedDate();
+        }
+    }
+}
