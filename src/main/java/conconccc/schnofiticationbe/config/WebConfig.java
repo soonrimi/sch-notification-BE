212e44d572de37d21b8114c188b844d494a28abb
conconcc <lesj9217@gmail.com>
2025-08-22 02:12:42 +0900

    fix : api prefix added on api-docs&swagger-ui

--- src/main/java/conconccc/schnofiticationbe/config/WebConfig.java
+++ src/main/java/conconccc/schnofiticationbe/config/WebConfig.java
@@ -1,6 +1,9 @@
 package conconccc.schnofiticationbe.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,5 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "https://notification.iubns.net/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer){
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
     }
 }
