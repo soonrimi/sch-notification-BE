a4de8275a3149766a593f3f26433376a9906e972
conconcc <lesj9217@gmail.com>
2025-09-11 00:27:16 +0900

    refactor : 출력을Categoryresponse로만 하도록 수정

--- src/main/java/com/schnofiticationbe/controller/SourceController.java
+++ src/main/java/com/schnofiticationbe/controller/SourceController.java
@@ -1,5 +1,9 @@
 package com.schnofiticationbe.controller;
 
+<<<<<<< HEAD
+=======
+import com.schnofiticationbe.dto.CategoryDto;
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
 import com.schnofiticationbe.entity.Category;
 import com.schnofiticationbe.service.CategoryService;
 import com.schnofiticationbe.service.CrawlPostService;
@@ -19,11 +23,16 @@ public class SourceController {
     private final CategoryService categoryService;
     //전체 카테고리 종류 조회
     @GetMapping("/tag")
+<<<<<<< HEAD
     public ResponseEntity<List<String>> getTags() {
         List<Category> categories = categoryService.getAllCategories();
         List<String> tags = categories.stream()
                 .map(category -> category.getCategoryName().toLowerCase(Locale.ROOT))
                 .toList();
         return ResponseEntity.ok(tags);
+=======
+    public ResponseEntity<List<CategoryDto.CategoryResponse>> getAllTags(){
+        return ResponseEntity.ok(categoryService.getAllCategories());
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
     }
 }
--- src/main/java/com/schnofiticationbe/dto/CategoryDto.java
+++ src/main/java/com/schnofiticationbe/dto/CategoryDto.java
@@ -1,5 +1,9 @@
 package com.schnofiticationbe.dto;
 
+<<<<<<< HEAD
+=======
+import com.schnofiticationbe.entity.Category;
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +14,11 @@ import lombok.Setter;
 @NoArgsConstructor // 기본 생성자
 @AllArgsConstructor
 
+<<<<<<< HEAD
 class CategoryDto {
+=======
+public class CategoryDto {
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
     private Long id;
     private String categoryName;
 
@@ -20,13 +28,23 @@ class CategoryDto {
     public static class UpdateRequest {
         private String categoryName;
     }
+<<<<<<< HEAD
+=======
+    @Getter
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
     public static class CategoryResponse {
         private Long id;
         private String categoryName;
 
+<<<<<<< HEAD
         public CategoryResponse(Long id, String categoryName) {
             this.id = id;
             this.categoryName = categoryName;
+=======
+        public CategoryResponse(Category category) {
+            this.id = category.getId();
+            this.categoryName = category.getCategoryName();
+>>>>>>> 0a5c4b4 (feat : 카테고리 종류 조회 api 추가)
         }
     }
 
