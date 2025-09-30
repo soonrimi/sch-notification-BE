ea3240f654d55b91d5025da9d506d496258f5a6f
conconcc <lesj9217@gmail.com>
2025-10-01 04:32:40 +0900

    feat : 로그 api추가

---
+++ src/main/java/com/schnofiticationbe/entity/Log.java
@@ -0,0 +1,71 @@
+package com.schnofiticationbe.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private LogLevel logLevel;
+
+    @Column(nullable = false, length = 64)
+    private String traceId;
+
+    private String clientIp;
+    private String requestUri;
+    private String httpMethod;
+
+    private String message;
+
+    @Lob
+    private String requestParams;
+
+    @Lob
+    private String requestBody;
+
+    @Lob
+    private String requestVariables;
+
+    @Lob
+    private String exceptionDetails;
+
+    private Integer httpStatus;
+    private Long durationMs;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt= Timestamp.valueOf(LocalDateTime.now());
+
+    @Builder
+    public Log(LogLevel logLevel, String traceId, String clientIp, String requestUri, String httpMethod,
+               String message, String requestParams, String requestBody, String requestVariables,String exceptionDetails,
+               Integer httpStatus, Long durationMs) {
+        this.logLevel = logLevel;
+        this.traceId = traceId;
+        this.clientIp = clientIp;
+        this.requestUri = requestUri;
+        this.httpMethod = httpMethod;
+        this.message = message;
+        this.requestParams = requestParams;
+        this.requestBody = requestBody;
+        this.requestVariables = requestVariables;
+        this.exceptionDetails = exceptionDetails;
+        this.httpStatus = httpStatus;
+        this.durationMs = durationMs;
+    }
+}
