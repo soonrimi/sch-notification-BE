2066c955a09ac93c8d075b8533b833429f5092fb
iubns <iubns@naver.com>
2025-09-30 23:20:00 +0900

    add: ParameterObject in pageable

--- src/main/java/com/schnofiticationbe/controller/CrawlPostController.java
+++ src/main/java/com/schnofiticationbe/controller/CrawlPostController.java
@@ -4,6 +4,7 @@ import com.schnofiticationbe.dto.NoticeDto;
 import com.schnofiticationbe.entity.Category;
 import com.schnofiticationbe.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class CrawlPostController {
 
 
     @GetMapping
-    public ResponseEntity<Page<NoticeDto.ListResponse>> getAllNotices(Pageable pageable) {
+    public ResponseEntity<Page<NoticeDto.ListResponse>> getAllNotices(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(noticeService.getCombinedNotices(pageable));
     }
 
@@ -30,15 +31,14 @@ public class CrawlPostController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Page<NoticeDto.ListResponse>> searchNotices(@RequestParam String keyword, Pageable pageable) {
+    public ResponseEntity<Page<NoticeDto.ListResponse>> searchNotices(@RequestParam String keyword, @ParameterObject Pageable pageable) {
         return ResponseEntity.ok(noticeService.searchNotices(keyword, pageable));
     }
 
     //카테고리별 공지사항 조회 (및 전체 조회)
     @GetMapping("/category")
     public ResponseEntity<Page<NoticeDto.ListResponse>> getNotices(
-            @RequestParam(required = false) Category category,
-            Pageable pageable){
+            @RequestParam(required = false) Category category,@ParameterObject Pageable pageable){
         Page<NoticeDto.ListResponse> postsPage;
 
         if (category != null) {
@@ -52,8 +52,7 @@ public class CrawlPostController {
 
     @GetMapping("/bookmark")
     public  ResponseEntity<Page<NoticeDto.ListResponse>> getNoticesByIds(
-            @RequestBody(required = false) List<Long> ids,
-            Pageable pageable) {
+            @RequestBody(required = false) List<Long> ids,@ParameterObject Pageable pageable) {
         Page<NoticeDto.ListResponse> postsPage = noticeService.getNoticesByIds(ids, pageable);
         return ResponseEntity.ok(postsPage);
     }
