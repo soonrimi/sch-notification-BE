2f31c8ba9d7e94e915396660613ce449986309ea
choco <kyw030906@gmail.com>
2025-11-08 01:49:39 +0900

    device id fix

--- src/main/java/com/schnofiticationbe/repository/KeywordRepository.java
+++ src/main/java/com/schnofiticationbe/repository/KeywordRepository.java
@@ -3,5 +3,10 @@ package com.schnofiticationbe.repository;
 import com.schnofiticationbe.entity.KeywordNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KeywordRepository extends JpaRepository<KeywordNotification, Integer> {
+
+    // device 기준 조회
+    List<KeywordNotification> findByDevice(String device);
 }
